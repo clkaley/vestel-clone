7c62c186ea729fa54f93445c36cb289be81b74fa
Aleyna Celik <clkkaley@gmail.com>
2022-08-18 11:08:40 +0300

    created navbar component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar";
 
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
   <>
     <Header/>
+    <Navbar/>
     <Footer/>
   </>
   );
